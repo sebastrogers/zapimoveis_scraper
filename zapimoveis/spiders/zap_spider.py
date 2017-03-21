2e85e33e916df88c3fc44b47c7b031ee04765e25
Paulo Romeira <paulo@pauloromeira.com>
2017-03-21 01:46:36 -0300

    Extract number of pages and loop

--- zapimoveis/spiders/zap_spider.py
+++ zapimoveis/spiders/zap_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy import Selector
 from scrapy_splash import SplashRequest
 
 
@@ -24,21 +25,29 @@ class ZapSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+
+        sel = Selector(response=response)
+        # <input type="hidden" id="quantidadeTotalPaginas" data-value="361" />
+        pags = int(sel.xpath('//input[@id="quantidadeTotalPaginas"]/@data-value').extract()[0])
+
         self.splash_count = 1
-        filename = 'response.html'
+        filename = 'files/response.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
 
-        yield SplashRequest(response.url, 
-                self.parse_splash,
-                endpoint='execute',
-                args={'lua_source': self.lua_script.format(pag=20, wait=2) },
-                dont_filter=True
-                )
+
+        # for pag in range(2, pags + 1):
+        for pag in range(2, 6):
+            yield SplashRequest(response.url, 
+                    self.parse_splash,
+                    endpoint='execute',
+                    args={'lua_source': self.lua_script.format(pag=pag, wait=2) },
+                    dont_filter=True
+                    )
 
     def parse_splash(self, response):
-        filename = 'response_splash{0}.html'.format(self.splash_count)
+        filename = 'files/response_splash{0}.html'.format(self.splash_count)
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
